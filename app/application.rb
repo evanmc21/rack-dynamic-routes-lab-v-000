c18123a3657c0241046091d4240e9e223498229c
Evan Candler <emcandler21@gmail.com>
2018-04-06 18:45:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,18 @@
 class Application
 
 @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item.each do |name|
+      item_name = req.path.split("/items/")
+      .each do |name|
         resp.write "#{name.price}"
+
+      else
+        resp.write "Route not found"
+        resp.status = 404
   end
 end
